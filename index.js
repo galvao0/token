04ead694bc985d39843b8721ac514f15d49ffee0
Pedro Nascimento Galvão <cin0127306@aluno.firjansenaisesi.com.br>
2026-03-27 15:08:59 -0300

    c

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const crypt = require('bcryptjs')
 const db = require('./db')
+const jwt = require('jsonwebtoken')
+
+const authMiddleware = require('./authMiddleware')
+const SECRET = "segredo"
 
 const app = express()
 
@@ -15,7 +19,53 @@ app.post('/register', async (req, res) => {
 
     const cmd = 'INSERT INTO credencial (login, senha) VALUES (?, ?)'
 
-    db.query(cmd, [login, senhaHash])
+    db.query(cmd, [login, senhaHash], (err, result) => {
+        if(err) {
+            return res.json({ msg: 'erro ao inserir' })
+        }
+        res.json({ msg: 'inserido' })
+    })
+})
+
+app.post('/login', async (req, res) => {
+    try {
+        const { login, senha } = req.body
+        const searchUser = db.query('SELECT * FROM credencial WHERE login = ?', login)
+        if (!searchUser) {
+            return res.json({ error: 'usuario nao encontardo' })
+        }
+        const verifPass = crypt.compare(senha, searchUser.senha)
+        if(!verifPass) {
+            return res.status(400).json({ error: 'senha inválida' })
+        }
+        res.json({
+            msg: 'usuario logado com sucesso',
+            user: {
+                id: searchUser.id,
+                login: searchUser.login
+            }
+        })
+    } catch (err) {
+        res.json({ error: err })
+    }
+})
+
+const token = jwt.sign(
+    {
+        id: user.id,
+        login: user.login
+    },
+    SECRET,
+    {
+        expiresIn: "1h"
+    }
+)
+
+app.get('/admin', authMiddleware, (req, res) => {
+    res.json({
+        msg: 'bem-vindo, admin',
+        user: req.user
+    })
 })
 
 app.listen(3030, () => {
